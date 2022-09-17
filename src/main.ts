0e131ba5f087c8963e7c659f82ae303bf1eba5f3
zhuangxingpeng <dy_zxp@163.com>
2022-09-18 04:15:55 +0800

    chore: vuex => pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.tsx'
 import router from '@/router'
-import store from "@/store"
 import "./app.scss"
 import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
 
-createApp(App).use(ElementPlus).use(router).use(store).mount('#app')
+createApp(App).use(ElementPlus).use(router).use(createPinia()).mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,5 @@
-import { createStore } from "vuex";
+import { createPinia } from "pinia";
 
-const store = createStore({
-	state:{},
-	getters:{},
-	mutations:{},
-	actions:{},
-	modules:{}
-})
+const store = createPinia()
 
 export default store
